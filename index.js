8714b7dd34472cc55c835fe39fd2919aff23425e
nmatos46 <nmatos419@hotmail.com>
2018-05-27 13:25:33 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,8 @@
+function theBeatlesPlay(musicPPL,musicTools){
+  var str = ""
+  var arr = []
+  for (var i=0; i<musicPPL.length; i++){
+    str = musicPPL[i] + " plays " + musicTools[i]
+    arr.push()
+  }
+}
