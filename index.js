653ecdd2794b6d949c29e0cd652c3ce6bce04e64
nmatos46 <nmatos419@hotmail.com>
2018-05-27 13:39:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 function theBeatlesPlay(musicPPL,musicTools){
-  var str = ""
-  var arr = []
+  var arr = [] //to hold all strings created in for loop
+  var str = "" //to hold strings to push into arr
+  
   for (var i=0; i < musicPPL.length; i++){
     str = musicPPL[i] + " plays " + musicTools[i]
     arr.push(str)
   }
   return arr
+}
+
+function johnLennonFacts(facts){
+  var arr = [] //to hold all strings created in while loop
+  var str = "" //to hold string created in one loop iteration
+  var i = 0 //counter used in while loop condition statement
+ 
+  while (i < facts.length){
+    arr.push(facts[i]) 
+    i++
+  }
 }
